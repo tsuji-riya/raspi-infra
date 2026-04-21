fbe33817a6af45a0c00f9d66abbc2572afb5806f
Tsuji Riya <tsuji.riya@gmail.com>
2026-04-21 12:46:41 +0900

    Add destination configuration for Cloudflare application

--- terraform/cloudflare_application.tf
+++ terraform/cloudflare_application.tf
@@ -3,6 +3,10 @@ resource "cloudflare_zero_trust_access_application" "raspi-k3s-api" {
   type = "self_hosted"
   zone_id = local.cloudflare_zone_id
   app_launcher_visible = false
+  destinations = [{
+    type = "public"
+    uri = "raspi-k3s-api.riya.work"
+  }]
   policies = [{
     id = cloudflare_zero_trust_access_policy.raspi-k3s-api.id
     precedence = 1
