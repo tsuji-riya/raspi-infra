0621e08c379cfa5bd40ea0ad252276f425108e86
Tsuji Riya <tsuji.riya@gmail.com>
2026-04-21 12:47:09 +0900

    Update Cloudflare access policy decision to non_identity(means service auth)

--- terraform/cloudflare_policy.tf
+++ terraform/cloudflare_policy.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_zero_trust_access_policy" "raspi-k3s-api" {
   account_id = local.cloudflare_account_id
   name       = "raspi-k3s-api"
-  decision = "allow"
+  decision = "non_identity"
   include = [
     {
       service_token = {
